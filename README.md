eb3256581013f1781fe9a7e1c5babb06188aedba
pylover <vahid.mardani@gmail.com>
2018-09-08 00:25:32 +0430

    Restructuring with git submodules

--- README.md
+++ README.md
@@ -7,27 +7,9 @@ sudo apt-get remove modemmanager
 ## Building the cross compile environment
 
 
-### Clone and install the open-sdk
+### Clone and install
 
 ```bash
-git clone --recursive https://github.com/pfalcon/esp-open-sdk.git
-cd exp-open-sk
-make
-```
-
-Create bash script to source and add the tool chain binaries into `PATH`
-
-```bash
-echo '''
-#! /usr/bin/env bash
-export PATH=$PWD/xtensa-lx106-elf/bin:$PATH
-''' > activate.sh
-chmod +x activate.sh
-```
-
-Source the newly created script update the `PATH` variable:
-
-```bash
-. activate.sh
+git clone --recursive git@github.com:pylover/esp.git
 ```
 
--- activate.sh
+++ activate.sh
@@ -1,2 +1,37 @@
-source ../esp-open-rtos/activate.sh
+
+if [ $0 = $BASH_SOURCE ]; then
+	echo "Can not run this script"
+	exit 1
+fi
+
+if [ ! -z $BACKUP_PATH ]; then
+	echo "Already sourced, run deactivate to allow source again."
+	return 1	
+fi
+
+if [ (! -z $SDK_PATH) || (! -z $BIN_PATH) ]; then
+	echo "Please set SDK_PATH and BIN_PATH"
+	return 1
+fi
+
+HERE=`dirname "$(readlink -f "$BASH_SOURCE")"`
+TITLE=`basename "$HERE"`
+echo $HERE
+echo $TITLE
+export BACKUP_PATH=$PATH
+export BACKUP_PS1=$PS1
+#export SDK_PATH="$HERE/ESP8266_NONOS_SDK"
+#export BIN_PATH="$here/bin"
+export PATH="$HERE/xtensa-toolchain/bin:$PATH"
+export PS1="($TITLE) $PS1"
+
+function deactivate {
+	export PATH=$BACKUP_PATH
+	export PS1=$BACKUP_PS1
+	unset BACKUP_PATH
+	unset BACKUP_PS1
+	unset SDK_PATH
+	unset BIN_PATH
+	unset -f deactivate
+}
 
