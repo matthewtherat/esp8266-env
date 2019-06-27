cddee542e6d7caaecb40a5ba59900832df215e1b
pylover <vahid.mardani@gmail.com>
2019-06-27 22:22:05 +0430

    refactoring

--- nonos/activate.sh
+++
@@ -1,7 +0,0 @@
-HERE=`dirname "$(readlink -f "$BASH_SOURCE")"`
-export SDK_PATH="$HERE/sdk"
-export BIN_PATH="$HERE/bin"
-export ENV_TITLE=`basename "$HERE"`
-mkdir -p "$HERE/bin"
-source $HERE/../activate.sh
-
