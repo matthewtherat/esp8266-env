1e6535ccdfab352b47083abdd395164d845d50eb
pylover <vahid.mardani@gmail.com>
2021-03-23 08:03:50 +0430

    Update newproject script's readme

--- scripts/new-project.sh
+++ scripts/new-project.sh
@@ -105,8 +105,12 @@ Or use predefined make macros:
 
 ```bash
 make clean
-make assets_map6user1
 make flash_map6user1 
+
+cd webui
+npm install
+cd ..
+make flash_map6webui
 
 ```
 ' > $DEST/README.md
